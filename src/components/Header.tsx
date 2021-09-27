976de8559466d95eade38d8b35f7ce441b80d0d8
GrayD77 <siarhei.drahichynski@gmail.com>
2021-09-27 20:18:49 +0300

    Add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Siarhei Drahichynski</h1>
+
+        </header>
+    );
+};
+
+export default Header;
